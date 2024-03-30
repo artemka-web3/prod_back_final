b2aca7429fbb5102f4464df7efdbdc4a08749201
artemka-web3 <artemkasidnev.web3@gmail.com>
2024-03-31 02:24:17 +0300

    models add resume.hackathon

--- xxprod/resumes/models.py
+++ xxprod/resumes/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from users.models import User
+from hackathons.models import Hackathon
 
 class Resume(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
+    hackathon = models.ForeignKey(Hackathon, on_delete = models.CASCADE)
     personal_website = models.CharField(max_length = 200, blank = True)
     github = models.CharField(max_length = 200, blank = True)
     hhru = models.CharField(max_length = 200, blank = True)
