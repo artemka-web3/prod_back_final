db02b83149d576e279746e23026691174e49a8a9
artemka-web3 <artemkasidnev.web3@gmail.com>
2024-03-31 16:24:10 +0300

    skeleton for hakatons and teams

--- hackathons/api.py
+++ hackathons/api.py
@@ -4,19 +4,22 @@ from .schemas import HackathonSchema, HackathonIn
 from .models  import Hackathon
 from accounts.models import Account
 from ninja import UploadedFile, File
+from authtoken import AuthBearer
+from xxprod.settings import SECRET_KEY
+from django.shortcuts import get_object_or_404
+import jwt
 
 
 hackathon_router = Router()
 my_hackathon_router = Router()
 
 
-@hackathon_router.post('/') # if is authed else 409
-def create_hackathon(request, payload: HackathonIn, image: UploadedFile = File(...)):
-    payload_dict = payload.dict()
-    hackathon = Hackathon(creator = request.user, name = payload_dict['name'], description = payload_dict['name'])
+@hackathon_router.post('/', auth = AuthBearer) # if is authed else 409
+def create_hackathon(request, body: HackathonIn, image: UploadedFile = File(...)):
+    body_dict = body.dict()
+    hackathon = Hackathon(creator = request.user, name = body_dict['name'], description = body_dict['name'])
     hackathon.image_cover.save(image.name, image)
-    for participant in payload_dict['participants']:
-        # try except throw 401
+    for participant in body_dict['participants']:
         participant_acc = Account.objects.get(email=participant)
         hackathon.participans.add(participant_acc)
     return hackathon
@@ -28,5 +31,8 @@ def list_hackathons(request):
 
 @my_hackathon_router.get("/", response=List[HackathonSchema])
 def list_myhackathons(request):
+    payload_dict = jwt.decode(request.auth, SECRET_KEY, algorithms=['HS256'])
+    user_id = payload_dict['user_id']
+    user = get_object_or_404(Account, id=user_id)
     hackathons = Hackathon.objects.filter(creator__id = request.user).all()
     return hackathons
--- teams/api.py
+++ teams/api.py
@@ -3,20 +3,47 @@ from typing import List
 from .schemas import TeamIn, TeamSchema
 from .models import Team
 from vacancies.models import Vacancy, Keyword
+from django.shortcuts import  get_object_or_404
+from accounts.models import Account
+import jwt
+from authtoken import AuthBearer
+from xxprod.settings import SECRET_KEY
 
 
 team_router = Router()
 
 
-@team_router.post("/create")
-def create_team(request, payload: TeamIn):
-    payload_dict = payload.dict()
-    team = Team(name = payload_dict['name'], creator = request.user)
-    for vacancy in payload_dict['vacancies_ids']:
+@team_router.post("/create", auth = AuthBearer)
+def create_team(request, body: TeamIn):
+    payload_dict = jwt.decode(request.auth, SECRET_KEY, algorithms=['HS256'])
+    user_id = payload_dict['user_id']
+    user = get_object_or_404(Account, id=user_id)
+    team = Team(name = body['name'], creator = user)
+    for vacancy in body['vacancies_ids']:
         vacancy = Vacancy.objects.create(team = team, name = vacancy['name'])
         for kw in vacancy['keywords']:
             Keyword.objects.create(vacancy = vacancy, text = kw)    
     return team
+
+
+@team_router.delete("/delete", auth = AuthBearer)
+def delete_team(request, hashed_id):
+    payload_dict = jwt.decode(request.auth, SECRET_KEY, algorithms=['HS256'])
+    user_id = payload_dict['user_id']
+    user = get_object_or_404(Account, id=user_id)
+    team = get_object_or_404(Team, id = user_id)
+    team.delete()
+    return {'success': True}
+
+@team_router.post('/invite-user')
+def invite_user(request, hashed_id):
+    payload_dict = jwt.decode(request.auth, SECRET_KEY, algorithms=['HS256'])
+    user_id = payload_dict['user_id']
+    user = get_object_or_404(Account, id=user_id)
+    decoded_id = 1
+    user = get_object_or_404(Account, id = decoded_id)
+    email = user.email
+
 
     
 
