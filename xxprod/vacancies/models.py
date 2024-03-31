96e16ccd79a6de55d10b2308edfcc43fa1cb13de
arklual <arklual1@gmail.com>
2024-03-31 10:45:56 +0300

    user integration with django build-in

--- xxprod/vacancies/models.py
+++ xxprod/vacancies/models.py
@@ -4,7 +4,7 @@ from accounts.models import Account
 
 class Vacancy(models.Model):
     name = models.CharField(max_length = 200, blank = False)
-    team = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Account, on_delete=models.CASCADE)
     
 
 class Keyword(models.Model):
