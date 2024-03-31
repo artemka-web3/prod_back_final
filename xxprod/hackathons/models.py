b419c3300ed96379e3f31adc0cebe7aa33fbc109
artemka-web3 <artemkasidnev.web3@gmail.com>
2024-03-31 11:13:53 +0300

    add hackathon description

--- xxprod/hackathons/models.py
+++ xxprod/hackathons/models.py
@@ -6,6 +6,7 @@ class Hackathon(models.Model):
     creator = models.ForeignKey(Account, on_delete = models.CASCADE)
     name = models.CharField(max_length = 200, blank = False)
     image_cover = models.ImageField(upload_to = 'hackathon_images/')
+    description = models.TextField(blank = False, default = 'описание хакатона')
 
 
 
