e8535280aa66f424e032333f589bb1aaf2741cec
arklual <arklual1@gmail.com>
2024-04-01 12:13:00 +0300

    fix add user to hack

--- hackathons/api.py
+++ hackathons/api.py
@@ -54,17 +54,23 @@ def list_hackathons(request):
     hackathons = Hackathon.objects.all()
     return 200, hackathons
 
-@hackathon_router.post("/{hackathon_id}/add_user/{user_id}", auth = AuthBearer(), response = {201: HackathonSchema, 401: Error, 404: Error, 403: Error, 400: Error})
-def add_user_to_hackathon(request, hackathon_id: int, user_id: int):
+@hackathon_router.post("/{hackathon_id}/add_user", auth = AuthBearer(), response = {201: HackathonSchema, 401: Error, 404: Error, 403: Error, 400: Error})
+def add_user_to_hackathon(request, hackathon_id: int, email: str):
     payload_dict = jwt.decode(request.auth, SECRET_KEY, algorithms=['HS256'])
     me_id = payload_dict['user_id']
     me = get_object_or_404(Account, id=me_id)
     hackathon = get_object_or_404(Hackathon, id=hackathon_id)
-    user_to_add = get_object_or_404(Account, id=user_id)
+    user_to_add = get_object_or_404(Account, email=email)
     if hackathon.creator == me:
         if user_to_add not in hackathon.participants.all() and user_to_add != hackathon.creator:
-            hackathon.participants.add(user_to_add)
-            hackathon.save()
+            encoded_jwt = jwt.encode({"createdAt": datetime.utcnow().timestamp(), "id": hackathon.id}, SECRET_KEY,
+                                     algorithm="HS256")
+            try:
+                send_mail(f"Приглашение в хакатон {hackathon.name}",
+                          f"https://prod.zotov.dev/join-hackaton?hackathon_id={encoded_jwt}", 'sidnevar@yandex.ru',
+                          [user_to_add.email], fail_silently=False)
+            except:
+                pass
             return 201, hackathon
         else:
             return 400, {'detail': "This user is already in hackathon"}
