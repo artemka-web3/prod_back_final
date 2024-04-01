6c187a0e9121d5ecac7aace496859627875e4c5d
artemka-web3 <artemkasidnev.web3@gmail.com>
2024-04-01 16:43:42 +0300

    migrate teams

--- teams/models.py
+++ teams/models.py
@@ -4,7 +4,7 @@ from hackathons.models import Hackathon
 
 
 class Team(models.Model):
-    hackathon = models.ForeignKey(Hackathon, on_delete = models.CASCADE)
+    hackathon = models.ForeignKey(Hackathon, on_delete = models.CASCADE, null = False)
     name = models.CharField(max_length = 200, blank = False)
     creator = models.ForeignKey(Account, on_delete = models.CASCADE)
     team_members = models.ManyToManyField(Account, related_name='team_members')
