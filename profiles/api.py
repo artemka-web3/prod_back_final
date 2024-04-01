5dd4d9c62b4ec918a4fceb59a92eff61e60c63d2
arklual <arklual1@gmail.com>
2024-04-01 14:30:16 +0300

    edit profile

--- profiles/api.py
+++ profiles/api.py
@@ -24,21 +24,12 @@ def profile_patch(request, user: UserProfileEdit):
     payload = jwt.decode(request.auth, SECRET_KEY, algorithms=['HS256'])
     user_id = payload['user_id']
     me = get_object_or_404(Account, id=user_id)
-    if user.email is not None:
-        me.email = user.email
-        me.save()
-    if user.age is not None:
-        me.age = user.age
-        me.save()
-    if user.city != '':
-        me.city = user.city
-        me.save()
-    if user.username is not None:
-        me.username = user.username
-        me.save()
-    if user.work_experience is not None:
-        me.work_experience = user.work_experience
-        me.save()
+    me.email = user.email
+    me.age = user.age
+    me.city = user.city
+    me.username = user.username
+    me.work_experience = user.work_experience
+    me.save()
     return 201, me
 
 @router.get('/profiles/{user_id}', response={200: UserProfile, 401: Error, 404: Error}, auth=AuthBearer())
